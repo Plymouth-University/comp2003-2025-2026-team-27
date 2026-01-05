00962e3eaf085ee251271860159960f0732938fd
B143real <benjmainaugustt02@gmail.com>
2026-01-05 20:26:41 +0000

    Add Login Razor Page UI matching legacy frmLogin layout

---
+++ SourceCode/LmsModernApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LmsModernApp.Areas.Identity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public class InputModel
+        {
+            public string UserName { get; set; } = "";
+            public string Password { get; set; } = "";
+        }
+
+        public InputModel Input { get; set; } = new();
+    }
+}
