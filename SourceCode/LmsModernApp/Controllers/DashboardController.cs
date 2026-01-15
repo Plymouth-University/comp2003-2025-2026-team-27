94799b3384dcfe0966270c07b20246c17a60b6a2
JaheimBlanche <jaheim.blanche@students.plymouth.ac.uk>
2026-01-15 05:57:48 -0400

    Dashboard Controller explanations

--- SourceCode/LmsModernApp/Controllers/DashboardController.cs
+++ SourceCode/LmsModernApp/Controllers/DashboardController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace LmsModernApp.Controllers
 {
+    //Handles dashboard page requests and preps data
     public class DashboardController : Controller
     {
         private readonly IDashboardRepository _repo;
@@ -17,11 +18,13 @@ namespace LmsModernApp.Controllers
 
         public async Task<IActionResult> Index(DateTime? from, DateTime? to)
         {
+            //Defaults to full range for issue daates if no range is provided
             var (minDate, maxDate) = await _repo.GetIssueDateRangeAsync();
 
             var fromDate = from ?? minDate;
             var toDate = to ?? maxDate;
 
+            //Swaps if user inputs dates in wrong oder
             if (fromDate > toDate)
             {
                 var tmp = fromDate;
@@ -33,6 +36,7 @@ namespace LmsModernApp.Controllers
             var totalBooks = await _repo.GetTotalBookCountAsync();
             var grouped = await _repo.GetIssuesByBorrowerGroupAsync(fromDate, toDate);
 
+            //Builds view model for dashboard UI and charts
             var vm = new DashboardViewModel
             {
                 TotalMemberCount = totalMembers,
